6163fd04decf7b289cfcfa052833648da27582f5
Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
2022-09-13 23:11:18 +0200

    outcommented lines regarding tile color and added a scene to the project

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -7,8 +7,8 @@ public class Tile : MonoBehaviour
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
 
-    public void Init(bool isOffset) {
-        _renderer.color = isOffset ? _offsetColor : _baseColor;
-    }
+    //public void Init(bool isOffset) {
+    //    _renderer.color = isOffset ? _offsetColor : _baseColor;
+    //}
   
 }
