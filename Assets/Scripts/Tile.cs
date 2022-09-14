2a493f4ae340cd863392c5444034295b05db536d
Christoffer <cph-cq2@cphbusiness.dk>
2022-09-14 11:32:26 +0200

    Added highlight to grid

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -6,7 +6,7 @@ public class Tile : MonoBehaviour
 {
     [SerializeField] private Color _baseColor, _offsetColor;
     [SerializeField] private SpriteRenderer _renderer;
-    //[SerializeField] private GameObject _highlight;
+    [SerializeField] private GameObject _highlight;
 
     //void Awake() 
     //{
@@ -16,13 +16,14 @@ public class Tile : MonoBehaviour
         GetComponent<Renderer>().material.color = isOffset ? _offsetColor : _baseColor;
     }
 
-    //void OnMouseEnter() {
-    //    _highlight.SetActive(true);
-    //}
+    void OnMouseEnter() {
+        Debug.Log(gameObject.name);
+        _highlight.SetActive(true);
+    }
 
-    //void OnMouseExit() {
-    //    _highlight.SetActive(false);
-    //}
+    void OnMouseExit() {
+        _highlight.SetActive(false);
+    }
 
     //public void SetColor()
     //{
