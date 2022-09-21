8a46f06fb167c90189d6c25c68f4f628472fa62a
Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
2022-09-21 10:35:55 +0200

    Added depth to GridManager and changed offset color to z axis

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-  [SerializeField] private int _width, _height;
+  [SerializeField] private int _width, _height, _depth;
 
   [SerializeField] private Tile _tilePrefab;
 
@@ -15,11 +15,13 @@ public class GridManager : MonoBehaviour
   void GenerateGrid() {
     for (int x = 0; x < _width; x++) {
         for (int y = 0; y < _height; y++) {
-            var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y), Quaternion.identity);
-            spawnedTile.name = $"Tile {x} {y}";
+          for (int z = 0; z < _depth; z++) {
+            var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y,z), Quaternion.identity);
+            spawnedTile.name = $"Tile {x} {y} {z}";
 
-            var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
+            var isOffset = (x % 2 == 0 && z % 2 != 0) || (x % 2 != 0 && z % 2 == 0);
             spawnedTile.Init(isOffset);
+            }
         }
     }
   }
