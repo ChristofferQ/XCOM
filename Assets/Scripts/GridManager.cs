33670cb5b6d6d4c82962482d75ccd5b76a651599
Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
2022-09-28 15:22:41 +0200

    hmm

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -8,14 +8,15 @@ public class GridManager : MonoBehaviour
   [SerializeField] private int _width, _height, _depth;
   [SerializeField] private Tile _tilePrefab;
 
+  private Dictionary<Vector2, Tile> _tiles;
+
   void Start() {
     GenerateGrid();
   }
 
   void GenerateGrid() {
+    _tiles = new Dictionary<Vector2, Tile>();
     for (int x = 0; x < _width; x++) {
-      //Add NavMesh to each tile
-      UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
         for (int y = 0; y < _height; y++) {
           for (int z = 0; z < _depth; z++) {
             var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y,z), Quaternion.identity);
@@ -24,10 +25,20 @@ public class GridManager : MonoBehaviour
             //Make every 2nd tile an offset color (Only works for x and z axis)
             var isOffset = (x % 2 == 0 && z % 2 != 0) || (x % 2 != 0 && z % 2 == 0);
             spawnedTile.Init(isOffset);
+
+
+            _tiles[new Vector2(x,z)] = spawnedTile;
             }   
         }
     }
-     //Add NavMesh
-            UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
+    //Add NavMesh
+    UnityEditor.AI.NavMeshBuilder.BuildNavMesh();    
   }
+
+  public Tile GetTileAtPosition(Vector2 pos) {
+    if(_tiles.TryGetValue(pos, out var tile)) {
+      return tile;
+    }
+    return null;
+  } 
 }
