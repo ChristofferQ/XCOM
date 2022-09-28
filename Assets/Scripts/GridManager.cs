9d61d7287d0185d3b89a3e654f2fa5fa547ed801
Christoffer <cph-cq2@cphbusiness.dk>
2022-09-28 10:18:06 +0200

    Moved NavMesh outside for loop

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -6,7 +6,6 @@ using UnityEditor.AI;
 public class GridManager : MonoBehaviour
 {
   [SerializeField] private int _width, _height, _depth;
-
   [SerializeField] private Tile _tilePrefab;
 
   void Start() {
@@ -20,14 +19,13 @@ public class GridManager : MonoBehaviour
             var spawnedTile = Instantiate(_tilePrefab, new Vector3(x,y,z), Quaternion.identity);
             spawnedTile.name = $"Tile {x} {y} {z}";
 
-            //Add NavMesh to each tile
-            UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
-
             //Make every 2nd tile an offset color (Only works for x and z axis)
             var isOffset = (x % 2 == 0 && z % 2 != 0) || (x % 2 != 0 && z % 2 == 0);
             spawnedTile.Init(isOffset);
-            }
+            }   
         }
     }
+     //Add NavMesh
+            UnityEditor.AI.NavMeshBuilder.BuildNavMesh();
   }
 }
