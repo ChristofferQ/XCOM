ccaededb861b123d5ed6bb80c6e0ec312c6e2177
Ermin Dzafic <71074936+ErminDZ@users.noreply.github.com>
2023-01-06 00:15:15 +0100

    Added toggle props on and off

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -1,42 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject gameTimer;
-   public void PlayGame()
-   {
+    public Toggle props;
+    public void PlayGame()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-   }
+    }
 
-   public void Credit()
+    public void Credit()
     {
         SceneManager.LoadScene("CreditScene");
     }
 
-   public void QuitGame()
-   {
+    public void QuitGame()
+    {
         Application.Quit();
-   }
+    }
 
-   public void BackToMainMenu()
+    public void BackToMainMenu()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
     }
 
-    public void NextLevel()
+    public void onProps()
     {
-        
+        if (props.isOn)
+        {
+            Debug.Log("on");
+            GridManager.props = true;
+        }
+        else
+        {
+            Debug.Log("off");
+            GridManager.props = false;
+        }
     }
     public void showGameTimer(bool tog)
     {
         if (tog == true)
         {
             gameTimer.SetActive(true);
-        }else
+        }
+        else
         {
             gameTimer.SetActive(false);
         }
