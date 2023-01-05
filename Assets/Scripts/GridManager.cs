601ab461b8b1563f80c0a7d87be1f6c302a50cf4
Christoffer Quakernaat <cph-cq2@cphbusiness.dk>
2023-01-05 14:49:09 +0100

    lowered tourches on walls

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -90,8 +90,8 @@ private void spawnProps()
       var spawnedWallProp1 = Instantiate(wallprop1, new Vector3(Random.Range(0, _width),4,-0.5f), Quaternion.identity);
       var spawnedWallProp2 = Instantiate(wallprop1, new Vector3(Random.Range(0, _width),4,_depth -0.5f), Quaternion.Euler(0,180,0));
 
-      var spawnedWallProp3 = Instantiate(wallprop2, new Vector3(-0.56f,3.5f,Random.Range(0, _depth)), Quaternion.Euler(0,90,0));
-      var spawnedWallProp4 = Instantiate(wallprop2, new Vector3(_width -0.44f,3.5f,Random.Range(0, _depth)), Quaternion.Euler(0,270,0));
+      var spawnedWallProp3 = Instantiate(wallprop2, new Vector3(-0.56f,2.5f,Random.Range(0, _depth)), Quaternion.Euler(0,90,0));
+      var spawnedWallProp4 = Instantiate(wallprop2, new Vector3(_width -0.44f,2.5f,Random.Range(0, _depth)), Quaternion.Euler(0,270,0));
     }
   }
 
